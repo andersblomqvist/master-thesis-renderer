8d1150986d7ce997b5bf058bde4f4064b5e6df70
Anders Blomqvist <anders@minaemail.se>
2025-04-22 16:44:04 +0200

    removed run command

--- RMSECalculator/build.bat
+++ RMSECalculator/build.bat
@@ -5,7 +5,3 @@ set PROJECT_DIR=.
 set SRC_DIR=%PROJECT_DIR%\src
 
 odin build src\ -microarch:native -collection:src=src -out:%EXE_NAME%.exe -o:speed  -no-bounds-check
-
-if "%1" == "run" (
-    %EXE_NAME%.exe C:\Users\ander\Documents\rmse-calculator\scene_1_blue_noise_ema_none
-)
